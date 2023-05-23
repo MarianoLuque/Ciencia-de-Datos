42c35ad7680749f66b62bfbc4a13b6fc8cae9519
MarianoLuque <mn.luque.14@gmail.com>
2023-05-22 23:59:24 -0300

    Graficos

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def leerArchivo():
     """Definir el archivo y leerlo"""
     filename = 'datos.csv'
     df = pd.read_csv(filename, header=0)
     return df
-def contar(df, columna):
+
+
+def contar(df, columna, desde, hasta):
     """
     Cuenta la cantidad de elementos y nulos de una columna
     Args:
         :param df: Dataframe utilizado
         :param columna: Columna que se quiere contar los registros
+        :param desde: Desde que nro de crimenes contar
+        :param hasta: Hasta que nro de crimenes contar
     """
     cantidad = df[columna].value_counts()
     nulos = df[columna].isnull().sum()
+    # Filtrar códigos de crimen con más de "desde" y menos de "hasta" crímenes asociados
+    if(desde != 0):
+        cantidad = cantidad[cantidad >= desde]
+    if(hasta != 0):
+        cantidad = cantidad[cantidad <= hasta]
     return (cantidad, nulos)
+
+
 def ordenar(tipoOrdenamiento, descendente, objeto):
     """
     Ordena los datos de acuerdo a los valores de X o de Y
@@ -25,12 +37,14 @@ def ordenar(tipoOrdenamiento, descendente, objeto):
     :param objeto: columna a ordenar
     """
     ascendente = True
-    if(descendente != True):
+    if (descendente != True):
         ascendente = False
     if tipoOrdenamiento == 1:
         return objeto.sort_index(ascending=ascendente)
     if tipoOrdenamiento == 2:
         return objeto.sort_values(ascending=ascendente)
+
+
 def rotular(grafico, titulo, labelX, labelY):
     """
     Agrega etiquetas al grafico
@@ -42,29 +56,51 @@ def rotular(grafico, titulo, labelX, labelY):
     grafico.set_title(titulo)
     grafico.set_xlabel(labelX)
     grafico.set_ylabel(labelY)
+
+
 def print_datos():
     df = leerArchivo()
-    cantidad, nulos = contar(df, "codigoCrimen1")
+    cantidad, nulos = contar(df, "edadVictima", 2300, 0)
     ordenados = ordenar(2, False, cantidad)
+    print(nulos)
+
+    # Calculando el porcentaje de registros con cantidad de crímenes dentro del rango
+    total_registros = len(df)
+    cantidad_rango = cantidad.sum() + nulos
+    print(cantidad_rango)  # Suma de la cantidad de crímenes dentro del rango
+    porcentaje_rango = (cantidad_rango / total_registros) * 100
+    
+    # Contando la cantidad de códigos de crimen dentro del rango
+    cantidad_codigos_rango = len(cantidad)
+
+    # Contando la cantidad de códigos de crimen fuera del rango (< 5000)
+    cantidad_codigos_fuera_rango = len(df["edadVictima"].unique()) - cantidad_codigos_rango
+
+    print(f"Porcentaje de registros con cantidad de crímenes dentro del rango: {porcentaje_rango:.2f}%")
+    print(f"Cantidad de códigos de crimen dentro del rango: {cantidad_codigos_rango}")
+    print(f"Cantidad de códigos de crimen fuera del rango (< 5000): {cantidad_codigos_fuera_rango}")
+    
 
     # ajustar para que se muestren todos los resultados por consola
     pd.set_option('display.max_rows', None)
-
+    pd.set_option('display.max_columns', None)
     # Generar el gráfico de barras
     grafico = ordenados.plot(kind='bar')
 
     # Modificando etiquetas de eje X (pone los nros verticales)
-    grafico.set_xticklabels(ordenados.index, rotation=0)
+    grafico.set_xticklabels(ordenados.index, rotation=90)
+    #Para no mostrar una etiqueta
+    #grafico.set_xticklabels([])
 
     # Agregar etiquetas con los valores de cada barra
     for i, v in enumerate(ordenados):
-        grafico.text(i, v, str(round(v, 2)), ha='center', fontweight='bold', fontsize=8)
+        grafico.text(i, v + 1000, str(round(v, 2)), ha='center', fontweight='bold', fontsize=8, rotation='vertical')
 
-    # Configurar el eje X y el título del gráfico
-    rotular(grafico, 'Cantidad de incidentes por área', 'Area', 'Cantidad de incidentes')
+    # Rotular los ejes y el título del gráfico
+    rotular(grafico, 'Cantidad de incidentes por estado', 'Estado', 'Cantidad de incidentes')
     # Mostrar grafico
-    print(df.describe())
     plt.show()
+
 
 def buscarVehiculos():
     # Definir el archivo y leerlo
@@ -76,6 +112,7 @@ def buscarVehiculos():
 
     # Imprime el resultado
     print(f"El archivo tiene {count} filas que contienen 'vehicle' en la columna 'CRM CD DESC'.")
+
 
 def ejemploProfe():
     # dado dos conjuntos de datos:
@@ -101,4 +138,3 @@ def ejemploProfe():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_datos()
-
