53c3d07e1d54354035d8663b7a6a3cc6592695bc
MarianoLuque <mn.luque.14@gmail.com>
2023-05-24 17:40:57 -0300

    mas graficos

--- main.py
+++ main.py
@@ -85,16 +85,6 @@ def filtrar_por_percentiles(df, columna, percentile_desde, percentile_hasta):
     df_filtrado = df.loc[(df[columna] >= valor_desde) & (df[columna] <= valor_hasta)]
     return df_filtrado
 
-def valoresAModificar():
-    columna = "fecha"
-    desde = 0
-    hasta = 0
-    percentile_desde = 0
-    percentile_hasta = 100
-    titulo = 'Cantidad de incidentes por mes'
-    ejeX = 'Mes'
-    ejeY = 'Cantidad de incidentes'
-    return columna, desde, hasta, titulo, ejeX, ejeY, percentile_desde, percentile_hasta
 
 def recortarHoras(df):
     df['hora'] = df['horaAcontecimiento'].apply(lambda x: str(x).zfill(4)[:2])
@@ -110,41 +100,96 @@ def agrupar_por_fecha(df):
     return df
 
 
-def filtrar_edad_victima(df, desde, hasta):
+def filtrar_por_edad(df, columna, desde, hasta):
     """
-    Filtra la columna 'edadVictima' de un DataFrame según los parámetros 'desde' y 'hasta'.
+    Filtra la columna 'edadVictima' de un DataFrame según los parámetros 'desde' y 'hasta',
+    y distribuye los registros de forma aleatoria dentro del rango especificado.
 
     Args:
         df (pandas.DataFrame): DataFrame a filtrar.
+        columna (str): Nombre de la columna que contiene las edades.
         desde (int): Valor mínimo para la edad.
         hasta (int): Valor máximo para la edad.
 
     Returns:
-        pandas.DataFrame: DataFrame filtrado por edadVictima.
+        pandas.DataFrame: DataFrame filtrado y distribuido aleatoriamente por edadVictima.
     """
-    df_filtrado = df.loc[(df['edadVictima'] >= desde) & (df['edadVictima'] <= hasta)]
+    df_filtrado = df.copy()
+    mask = (df_filtrado[columna] < desde) | (df_filtrado[columna] > hasta)
+    cantidad_registros = mask.sum()
+    valores_aleatorios = np.random.randint(desde, hasta + 1, size=cantidad_registros)
+    df_filtrado.loc[mask, columna] = valores_aleatorios
+
     return df_filtrado
 
 def graficarEntreVariables(df, bin_size):
-    sns.pairplot(data=df, vars=['edadVictima', 'codigoCrimen', 'areaNro'], markers='+', diag_kws={'bins': bin_size})
+    sns.pairplot(data=df, vars=['edadVictima', 'codigoCrimen'], markers='+', diag_kws={'bins': bin_size})
     plt.show()
+
+def graficoDeColores(df):
+    plt.figure(figsize=(10, 6))
+    sns.countplot(data=df, x='codigoCrimen', hue='areaNro')
+    plt.ylabel('Cantidad de Crimenes')
+    plt.xlabel('Codigo del crimen')
+
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., title='Numero de Area', ncol=2)
+    plt.subplots_adjust(bottom=0.1, top=0.9, left=0.1, right=0.8)  # Ajustar los márgenes según necesites
+
+    sns.despine(left=True)
+    plt.show()
+
+def filtrar_por_sexo(df):
+    df['sexoVictima'] = df['sexoVictima'].fillna('X')  # Rellenar valores nulos con 'X'
+    df['sexoVictima'] = df['sexoVictima'].replace(['H'], 'X')
+    filtro = (df['sexoVictima'] == 'M') | (df['sexoVictima'] == 'F') | (df['sexoVictima'] == 'X')
+    df_filtrado = df[filtro]
+    return df_filtrado
+
+def filtrar_por_codigo_crimen(df):
+    codigo_crimen = [510, 624, 354, 330, 740, 310, 230, 440, 626, 420, 331, 210, 341, 745, 930, 442, 761, 888, 236, 901, 480, 956, 946, 900]
+    df_filtrado = df[df['codigoCrimen'].isin(codigo_crimen)]
+    return df_filtrado
+
+def filtrar_por_descendencia(df):
+    df['descendenciaVictima'] = df['descendenciaVictima'].fillna('X')  # Rellenar valores nulos con 'X'
+    df['descendenciaVictima'] = df['descendenciaVictima'].replace(['K', 'F', 'C', 'J', 'V', 'I', 'Z', 'P', 'U', 'D', 'G', 'L', 'S', '-'], 'O')
+    descendencia = ['H', 'W', 'B', 'X', 'O', 'A']
+    df_filtrado = df[df['descendenciaVictima'].isin(descendencia)]
+    return df_filtrado
+
+def valoresAModificar():
+    columna = "sexoVictima"
+    desde = 0
+    hasta = 0
+    percentile_desde = 0
+    percentile_hasta = 100
+    titulo = 'Cantidad de incidentes por sexo de la victima'
+    ejeX = 'Sexo de la victima'
+    ejeY = 'Cantidad de incidentes'
+    return columna, desde, hasta, titulo, ejeX, ejeY, percentile_desde, percentile_hasta
+
 
 def main():
     df = leerArchivo()
     df_filtrado = df
     columna, desde, hasta, titulo, ejeX, ejeY, percentile_desde, percentile_hasta = valoresAModificar()
-    df = agrupar_por_fecha(df)
-    df_filtrado = recortarHoras(df)
-    df_filtrado = filtrar_edad_victima(df, 18, 75)
-    cantidad, nulos = contar(df, columna, desde, hasta)
+    df_filtrado = agrupar_por_fecha(df_filtrado)
+    df_filtrado = recortarHoras(df_filtrado)
+    df_filtrado = filtrar_por_sexo(df_filtrado)
+    df_filtrado = filtrar_por_codigo_crimen(df_filtrado)
+    df_filtrado = filtrar_por_descendencia(df_filtrado)
+    df_filtrado = filtrar_por_edad(df_filtrado, 'edadVictima', 12, 76)
+    cantidad, nulos = contar(df_filtrado, columna, desde, hasta)
     ordenados = ordenar(2, False, cantidad)
 
+    graficoDeColores(df_filtrado)
+
     # Datos para calcular porcentajes
-    totalRegistros = len(df) # cantidad de datos que tiene el registro
+    totalRegistros = len(df_filtrado) # cantidad de datos que tiene el registro
     cantidadRegistrosRango = cantidad.sum() # cantidad de datos dentro de desde-hasta
     cantidadRegistrosFueraRango = totalRegistros - cantidadRegistrosRango - nulos
     cantidadValoresRango = len(cantidad) # cantidad de valores unicos dentro de desde-hasta
-    cantidadValoresFueraRango = len(df[columna].unique()) - cantidadValoresRango # cantidad de valores unicos fuera de desde-hasta
+    cantidadValoresFueraRango = len(df_filtrado[columna].unique()) - cantidadValoresRango # cantidad de valores unicos fuera de desde-hasta
     porcentajeNulos, porcentajeEnRango, porcentajeFueraRango = calcularPorcentajes(totalRegistros, nulos, cantidadRegistrosRango, cantidadRegistrosFueraRango)
 
     print(f"**Nulos**\nCantidad de registros con nulos: {nulos}")
@@ -176,9 +221,7 @@ def main():
     # Mostrar grafico
     plt.show()
 
-    graficarEntreVariables(df_filtrado, 20)
-    print(df['edadVictima'].describe())
-    print(df_filtrado['edadVictima'].describe())
+    # graficarEntreVariables(df_filtrado, 20)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
